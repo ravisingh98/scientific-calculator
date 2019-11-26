ce1dc4616702b6a9ecaf5829bff8cebe47f0f405
Ravi Singh <40501289+ravisingh98@users.noreply.github.com>
2019-11-26 22:44:36 +0530

    Add files via upload

---
+++ precache-manifest.d43dd6df49e4079e8032993aed24b5f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc4bce48ba203f8e9a8a214d99c8e00",
+    "url": "/scientific-calculator/index.html"
+  },
+  {
+    "revision": "d09a00f8f3b4fd2316c2",
+    "url": "/scientific-calculator/static/css/main.458a1aaf.chunk.css"
+  },
+  {
+    "revision": "9bae813dc5e52fa10b3b",
+    "url": "/scientific-calculator/static/js/2.eb3bec8d.chunk.js"
+  },
+  {
+    "revision": "d09a00f8f3b4fd2316c2",
+    "url": "/scientific-calculator/static/js/main.20129f81.chunk.js"
+  },
+  {
+    "revision": "71d5e843f7b55e68ddc8",
+    "url": "/scientific-calculator/static/js/runtime-main.036667eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81bdfcfbd09301883363f9c7dfe1b2d6.js"
+  "/scientific-calculator/precache-manifest.d43dd6df49e4079e8032993aed24b5f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scientific-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
