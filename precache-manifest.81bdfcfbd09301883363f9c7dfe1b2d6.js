92fb2a7ccc52bd689d66e114ff25026974ba24a7
Ravi Singh <40501289+ravisingh98@users.noreply.github.com>
2019-11-26 22:20:51 +0530

    Add files via upload

---
+++ precache-manifest.81bdfcfbd09301883363f9c7dfe1b2d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293815a8a032e8693096e6979c990ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d09a00f8f3b4fd2316c2",
+    "url": "/static/css/main.458a1aaf.chunk.css"
+  },
+  {
+    "revision": "9bae813dc5e52fa10b3b",
+    "url": "/static/js/2.eb3bec8d.chunk.js"
+  },
+  {
+    "revision": "d09a00f8f3b4fd2316c2",
+    "url": "/static/js/main.20129f81.chunk.js"
+  },
+  {
+    "revision": "da2961ea9664d3953bba",
+    "url": "/static/js/runtime-main.2238a7c0.js"
+  }
+]);
